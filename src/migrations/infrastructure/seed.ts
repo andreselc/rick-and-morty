837e75dc6ee23faccddfb934a6817fe9de360c1d
andreselc <aelopez.20@est.ucab.edu.ve>
2024-10-08 01:15:39 -0400

    seed has now creation of seasons inside subcategories

--- src/migrations/infrastructure/seed.ts
+++ src/migrations/infrastructure/seed.ts
@@ -67,6 +67,47 @@ export class Seed {
         });
     }
 
+    //Inserción de datos en SubCategoria
+
+    //Primero se verifica si ya hay datos dentro de la tabla subcategoria
+    const subCategoryCount = await this.prisma.sub_Category.count();
+
+    //Se obtiene el atributo para sacar su ID.
+    if (subCategoryCount === 0) {
+      const speciesCharacter = await this.prisma.sub_Category.findFirst({
+        where: { name: 'species' },
+      });
+      const seasonEpisode = await this.prisma.sub_Category.findFirst({
+        where: { name : 'season' },
+      });
+
+
+    // Llamar a un endpoint para obtener datos de las temporadas y especies de personajes
+    const charactersResponse = await axios.get('https://rickandmortyapi.com/api/character');
+    const episodesResponse = await axios.get('https://rickandmortyapi.com/api/episode');
+
+    const characters = charactersResponse.data.results;
+    const episodes = episodesResponse.data.results;
+
+    //Procesar episodios para obtener todas las temporadas:
+
+    const seasons = new Set<string>();
+    episodes.forEach((episode: any) => {
+        const seasonCode = episode.episode.split('E')[0]; // Obtener la temporada del campo 'episode'
+        const seasonNumber = parseInt(seasonCode.replace('S', ''), 10); // Extraer el número de la temporada
+        const seasonName = `Season ${seasonNumber}`; // Formatear el nombre de la temporada
+        seasons.add(seasonName);
+    });
+
+    // Inserción de datos en sub_Category para temporadas
+    for (const season of seasons) {
+        await this.prisma.sub_Category.create({
+        data: { name: season, category_id: seasonEpisode.id },
+        });
+    }
+
+
+    }
     console.log('Proceso seeding completado');
   }
 
